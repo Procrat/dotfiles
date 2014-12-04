d43dfb016a4ef3a45a6d33712c85d1deca589f26
Stijn Seghers <seghers_stijn@hotmail.com>
2014-12-04 16:08:42 +0100

    Matplotlib as default import in ipython

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -5,5 +5,6 @@ c.TerminalIPythonApp.display_banner = False
 c.TerminalIPythonApp.quick = True
 c.InteractiveShellApp.exec_lines = [
     'from collections import *',
-    'from itertools import *'
+    'from itertools import *',
+    'from matplotlib import pyplot as plt'
 ]
