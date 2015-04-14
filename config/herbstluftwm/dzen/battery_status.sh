f23159344c1397e7d3ec6cc04786fd1a1eb1ee9d
Stijn Seghers <seghers_stijn@hotmail.com>
2015-04-14 13:11:23 +0200

    Show battery capacity when charging but not at 100%

--- config/herbstluftwm/dzen/battery_status.sh
+++ config/herbstluftwm/dzen/battery_status.sh
@@ -34,7 +34,11 @@ capacity_icon() {
 }
 
 if [[ $(cat $ac_info/online) -eq 1 ]]; then
-    echo "^fg($SECONDARY_CONTENT_COLOR)^pa(;4)^i($icon_dir/ac_02.xbm)^fg()"
+    echo -n "^fg($SECONDARY_CONTENT_COLOR)^pa(;4)^i($icon_dir/ac_02.xbm)"
+    capacity=$(cat $battery_info/capacity)
+    if [[ $capacity -lt 100 ]]; then
+        echo -n " ^pa(;0)$capacity%"
+    fi
 elif [[ $(cat $battery_info/present) -eq 0 ]]; then
     echo 'Wtf, you don'\''t work on AC and you don'\''t have a battery?! o.0' >&2
     exit 1
@@ -42,5 +46,7 @@ else
     capacity=$(cat $battery_info/capacity)
     color=$(capacity_color $capacity)
     icon=$(capacity_icon $capacity)
-    echo "^fg($color)^pa(;4)^i($icon) ^pa(;0)$capacity%^fg()"
+    echo -n "^fg($color)^pa(;4)^i($icon) ^pa(;0)$capacity%"
 fi
+
+echo '^fg()'
