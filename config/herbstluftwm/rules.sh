09c7a5414c2a50f1f0b3e294b3466cdb73e7bc90
Stijn Seghers <seghers_stijn@hotmail.com>
2015-08-08 07:42:54 +0200

    Turned off pseudotiling for Deluge and Minecraft

--- config/herbstluftwm/rules.sh
+++ config/herbstluftwm/rules.sh
@@ -74,7 +74,8 @@ hc rule instance='Mail' tag=mail
 # Move browsers to 'web' tag
 hc rule instance~'Navigator|Opera' tag=web
 # Turn off default pseudotiling for some programs
-hc rule class~'Firefox|Opera|Chromium|Thun(ar|derbird)|Vlc|mpv|feh' \
+hc rule
+class~'Firefox|Opera|Chromium|Thun(ar|derbird)|Vlc|mpv|feh|Deluge|Minecraft.*' \
     pseudotile=off
 
 # Start on 'terms' tag
