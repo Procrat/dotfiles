db84d0da15fed552f5f5da6a2559eb3727674d40
Stijn Seghers <seghers_stijn@hotmail.com>
2016-02-20 12:25:19 +0100

    Cleanup of profiling script

--- bin/profile_bash.sh
+++ bin/profile_bash.sh
@@ -2,15 +2,12 @@
 #
 # Assumes file descriptor 3 is not messed with in the profiled script.
 
-pid=$$
-exec 3>&2 2> >(tee /tmp/bash_profile-${pid}.log | \
+exec 3>&2 2> >(tee /tmp/bash-profile-$$.log | \
                sed -u 's/^.*$/now/' | \
-               date -f - +%s.%N >/tmp/bash_profile-${pid}.tim)
+               date -f - +%s.%N >/tmp/bash-profile-$$.tim)
 set -x
 
-command="$1"
-shift
-. "$command" "$@"
+. "$@"
 
 set +x
 exec 2>&3 3>&-
@@ -20,8 +17,8 @@ paste <(
         crt=000000000$((${tim//.}-10#0$last))
         printf "%12.9f\n" ${crt:0:${#crt}-9}.${crt:${#crt}-9}
         last=${tim//.}
-    done < /tmp/bash_profile-${pid}.tim
-) /tmp/bash_profile-${pid}.log > ${pid}.timings
+    done < /tmp/bash-profile-$$.tim
+) /tmp/bash-profile-$$.log > $$.timings
 
-rm /tmp/bash_profile-${pid}.{tim,log}
-echo ${pid}.timings
+rm /tmp/bash-profile-$$.{tim,log}
+echo $$.timings
