011c91836ffeb5fb19fdc3cefad67a17cd38797b
Stijn Seghers <seghers_stijn@hotmail.com>
2016-03-13 10:41:33 +0100

    Stripped down battery indicator script

--- config/herbstluftwm/dzen/battery_status.sh
+++ config/herbstluftwm/dzen/battery_status.sh
@@ -9,8 +9,10 @@ source $HOME/.colors
 battery=/sys/class/power_supply/BAT0
 icon_dir=$HOME/.config/icons/xbm
 
+status="$(cat $battery/status)"
+capacity="$(cat $battery/capacity)"
+
 capacity_color() {
-    capacity=$1
     if [[ $capacity -gt 66 ]]; then
         echo $OK_COLOR
     elif [[ $capacity -gt 33 ]]; then
@@ -21,28 +23,23 @@ capacity_color() {
 }
 
 capacity_icon() {
-    capacity=$1
-    rounded=$(echo "($capacity + 5) / 10 * 10" | bc)
+    local rounded=$(echo "($capacity + 5) / 10 * 10" | bc)
     # We only have icons for 10 to 90 (not for 0 and 100)
     if [[ $rounded -lt 10 ]]; then
         rounded=10
     elif [[ $rounded -gt 90 ]]; then
         rounded=90
     fi
-    echo $icon_dir/battery${rounded}.xbm
+    echo "$icon_dir/battery${rounded}.xbm"
 }
 
-if [[ "$(cat $battery/status)" = 'Charging' ]]; then
+if [[ "$status" = 'Full' ]]; then
     echo -n "^fg($SECONDARY_CONTENT_COLOR)^p(;+5)^i($icon_dir/ac_02.xbm)^p()"
-    capacity=$(cat $battery/capacity)
-    if [[ $capacity -lt 100 ]]; then
-        echo -n " $capacity%"
-    fi
-else
-    capacity=$(cat $battery/capacity)
-    color=$(capacity_color $capacity)
-    icon=$(capacity_icon $capacity)
-    echo -n "^fg($color)^p(;+5)^i($icon)^p() $capacity%"
+elif [[ "$status" = 'Charging' ]]; then
+    echo -n "^fg($SECONDARY_CONTENT_COLOR)^p(;+5)^i($icon_dir/ac_02.xbm)^p()"
+    echo -n " $capacity%"
+else  # Discharging
+    echo -n "^fg($(capacity_color))^p(;+5)^i($(capacity_icon))^p() $capacity%"
 fi
 
 echo '^fg()'
