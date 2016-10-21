4c458624bd96d6fa5b469506cc67a18fab830892
Stijn Seghers <stijnseghers@gmail.com>
2016-10-21 13:54:05 +0200

    Set PulseAudio fallback device when connecting to Bluetooth music device

--- bin/bt-connect.sh
+++ bin/bt-connect.sh
@@ -5,19 +5,23 @@
 set -euo pipefail
 
 
-declare -A DEVICES
-DEVICES['Nude Super-M']='A0:E9:DB:5C:FD:44'
-DEVICES['Zeus']='60:E3:27:09:12:DE'
+declare -A MAC_ADDRESSES PULSE_SINKS
+MAC_ADDRESSES['Nude Super-M']='A0:E9:DB:5C:FD:44'
+MAC_ADDRESSES['Zeus']='60:E3:27:09:12:DE'
+PULSE_SINKS['Nude Super-M']='bluez_sink.A0_E9_DB_5C_FD_44'
+declare -a DEVICES=("${!MAC_ADDRESSES[@]}")
 
-
-mac_address() {
-    local chosen_device=$(printf '%s\n' "${!DEVICES[@]}" | mydmenu)
-    echo "${DEVICES[$chosen_device]}"
-}
 
 rfkill unblock bluetooth
 systemctl is-active bluetooth >/dev/null || sudo systemctl start bluetooth
+
+device=$(printf '%s\n' "${DEVICES[@]}" | mydmenu)
+mac_address="${MAC_ADDRESSES[$device]}"
+pulse_sink="${PULSE_SINKS[$device]:-}"
+
 {
     echo 'power on'
-    echo -e "connect $(mac_address)"
+    echo -e "connect $mac_address"
 } | bluetoothctl -a
+
+[[ -n "$pulse_sink" ]] && pactl set-default-sink "$pulse_sink"
