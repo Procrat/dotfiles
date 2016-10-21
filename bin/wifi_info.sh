36da79e091621897617c40f5fba18c98d49464ef
Stijn Seghers <stijnseghers@gmail.com>
2016-10-21 13:55:19 +0200

    Yeah, don't ask

--- bin/wifi_info.sh
+++ bin/wifi_info.sh
@@ -5,6 +5,13 @@
 set -euo pipefail
 
 WIFI_DEVICE='wlp58s0'
+if ! iw dev "$WIFI_DEVICE" info; then
+    WIFI_DEVICE='wlan0'
+    if ! iw dev "$WIFI_DEVICE" info; then
+        echo 'No wifi device found' >&2
+        exit 1
+    fi
+fi
 
 
 # Shows SSID
