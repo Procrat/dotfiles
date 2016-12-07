154d6de11039689c0bfeeb02438ad869b2514bb9
Stijn Seghers <stijnseghers@gmail.com>
2016-12-08 00:25:02 +1300

    Make volume & brightness control wm independent

--- xmonad/dzen/dbrightness.sh
+++ xmonad/dzen/dbrightness.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 SECS=1
 ICON_FOLDER=~/.config/icons/xbm
 PIPE="/tmp/dbrightnesspipe"
+WIDTH=124
 
 dzen_dir=$(dirname $BASH_SOURCE)
 height=$(($(cat "$dzen_dir/panel_height") - 1))
@@ -28,12 +29,10 @@ ICON="$ICON_FOLDER/brightness.xbm"
 if [ ! -e "$PIPE" ]; then
     mkfifo "$PIPE"
 
-    geometry=( $(herbstclient monitor_rect 0) )
-    screen_x=${geometry[0]}
-    screen_y=${geometry[1]}
-    screen_width=${geometry[2]}
-    dzen_x=$(((screen_width - 124) / 2))
-    (dzen2 -x $dzen_x -y $screen_y -w 124 -h $height < "$PIPE"; rm -f "$PIPE") &
+    geometry="$(wmctrl -d | awk '/^...\*/ { print $9 }')"
+    screen_width="${geometry%x*}"
+    dzen_x=$(((screen_width - WIDTH) / 2))
+    (dzen2 -x $dzen_x -w $WIDTH -h $height < "$PIPE"; rm -f "$PIPE") &
 fi
 
 # Feed the pipe!
