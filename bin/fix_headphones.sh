02c34c5894983bb43442583374a2cfd33f6cf524
Stijn Seghers <stijnseghers@gmail.com>
2017-01-05 21:10:47 +1300

    Make sure stereo channels are the same volume

--- bin/fix_headphones.sh
+++ bin/fix_headphones.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #
 # Removes white noise while wearing headphones on Dell XPS 13
+# Also fixes the strange issue where the stereo channels don't have the same
+# volume.
 
 
 amixer -c 0 set 'Headphone Mic Boost' 1%
+
+volume=$(amixer get Master | grep -m1 -o '[0-9]\+%')
+amixer set Master "$volume"
