e2616333d1e281746a8108024f98bfef263f4b31
Stijn Seghers <stijnseghers@gmail.com>
2017-01-15 11:30:20 +1300

    Show EXIF data while browsing with feh

--- bin/feh_browser.sh
+++ bin/feh_browser.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+#
+# Browse images in a folder starting with the specified file onwards.
 
+set -euo pipefail
 shopt -s nullglob
 
-if [[ ! -f $1 ]]; then
-	echo "$0: first argument is not a file" >&2
-	exit 1
+FEH_OPTIONS=(--info 'exiv2 %F')
+
+if [[ -z "$1" || ! -f "$1" ]]; then
+    echo "$0: First argument is not a file" >&2
+    exit 1
 fi
 
 file=$(basename -- "$1")
 dir=$(dirname -- "$1")
-arr=()
 shift
 
 cd -- "$dir"
 
+arr=()
 for i in *; do
-	[[ -f $i ]] || continue
-	arr+=("$i")
-	[[ $i == $file ]] && c=$((${#arr[@]} - 1))
+    [[ -f $i ]] || continue
+    arr+=("$i")
+    [[ $i == $file ]] && c=$((${#arr[@]} - 1))
 done
 
-exec feh "$@" -- "${arr[@]:c}" "${arr[@]:0:c}"
+exec feh "${FEH_OPTIONS[@]}" "$@" -- "${arr[@]:c}" "${arr[@]:0:c}"
