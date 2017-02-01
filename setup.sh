2f30a59c99bd6d42201768ae23ac65d5aa06612f
Stijn Seghers <stijnseghers@gmail.com>
2017-02-02 10:44:54 +1300

    Added pylintrc

--- setup.sh
+++ setup.sh
@@ -56,6 +56,7 @@ dotfiles=(
     config/mimeapps.list
     config/nvim/init.vim
     config/nvim/ycm_extra_conf.py
+    config/pylintrc
     config/ranger/rc.conf
     config/systemd
     config/user-dirs.dirs
