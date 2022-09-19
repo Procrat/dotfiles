b4e1000342a6fa4e1125d8ac0abd5c0b9fd325bb
Stijn Seghers <stijnseghers@gmail.com>
2022-09-19 15:10:26 +0200

    IPython: don't warn when in virtualenv

--- config/ipython/profile_default/ipython_config.py
+++ config/ipython/profile_default/ipython_config.py
@@ -2,6 +2,7 @@ c.TerminalInteractiveShell.auto_match = True
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalIPythonApp.display_banner = False
 c.TerminalIPythonApp.quick = True
+c.InteractiveShell.warn_venv = False
 c.InteractiveShellApp.exec_lines = [
     'from collections import *',
     'from itertools import *',
